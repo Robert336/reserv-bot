ef7191826ba609a42d895d2ece2360c2b05940b5
Robert336 <rmazza33@gmail.com>
2021-08-22 18:20:35 -0400

    Create scrapeTable function.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
+// note: sraper should run every night at some time around 23:00 - 23:59
+
+
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+//     functions.logger.info("Hello logs!", { structuredData: true });
+//     response.send("Hello from Firebase!");
 // });
+
